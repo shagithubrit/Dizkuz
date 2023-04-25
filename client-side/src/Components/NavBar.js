a42a7a05aa127859c7abe5ba71f0129003a8e425
adityarai0705 <aditya70544@icloud.com>
2023-04-25 19:45:07 +0530

    formated code

--- client-side/src/Components/NavBar.js
+++ client-side/src/Components/NavBar.js
@@ -1,9 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from "react-router-dom";
 import './Components.css';
 
 function NavBar() {
+  const navigate = useNavigate();
+  const logout = async() => {
+    await localStorage.removeItem( 'currentUser');
+    navigate( '/landing');
+  }
+
+
   return (
     <div className='NavBarC'>
       <Navbar bg="dark" variant="dark">
@@ -13,6 +21,7 @@ function NavBar() {
             <Nav.Link href="/">Profile</Nav.Link>
             <Nav.Link href="/organisations">Organisations</Nav.Link>
             <Nav.Link href="/about-us">About Us</Nav.Link>
+            <Nav.Link onClick={logout}>Log out</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
